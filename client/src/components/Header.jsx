1543bba59c4033757239d613851afeab73b8227d
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-06-29 15:48:52 +0530

    Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { FaMoon } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const Header = () => {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-centered whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-2 bg-gradient-to-r from-gray-800 via-gray-600 to-gray-400 rounded-lg text-white">
+          Inked Insights
+        </span>
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill="true">
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button
+          className="w-12 h-10 hidden sm:inline "
+          color="gray"
+          pill="true"
+        >
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <button
+            className="px-2 py-2 bg-gradient-to-r from-gray-800 via-gray-600 to-gray-400 hover:from-gray-700 hover:via-gray-500 hover:to-gray-300 rounded-lg text-white shadow-md"
+            pill="true"
+          >
+            Sign In
+          </button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
