815be43a3c828acd48de24da722f2a43e3683956
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-06-30 15:29:05 +0530

    controller created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 const app = express();
 
 dotenv.config();
@@ -12,6 +13,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
