1101a51a03898974b747f0f2c92d2e5e943dfdc7
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-06-30 16:18:07 +0530

    create signup API

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 const app = express();
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -15,6 +18,7 @@ mongoose
   });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique },
-    email: { type: String, required: true, unique },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-const user = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
