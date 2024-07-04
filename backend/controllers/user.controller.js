bca8b4f1a0ab8999e80d8e883f3a5e76a6e05a9a
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-07-04 16:12:37 +0530

    delete account functionality added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,13 +55,17 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+  console.log(req.user);
+  if (req.user.id !== req.params.userId) {
+    console.log("User not authorized to delete this user");
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
     await User.findByIdAndDelete(req.params.userId);
+    console.log("User deleted successfully");
     res.status(200).json("User has been deleted");
   } catch (error) {
+    console.error("Error deleting user:", error);
     next(error);
   }
 };
