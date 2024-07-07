e4316eb1c497d6a7841dda832153661ddfbd22a9
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-07-07 14:51:05 +0530

    calltoaction added

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,26 @@
+import { Button } from "flowbite-react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <p className="text-gray-500 my-2">
+          Checkout these resources with 100 JavaScript Projects
+        </p>
+        <Button className=" bg-gradient-to-r from-yellow-500 via-yellow-350 to-yellow-300 hover:from-yellow-500 hover:via-yellow-350 hover:to-yellow-300 rounded-lg text-white shadow-md rounded-tl-xl rounded-bl-none">
+          <a
+            href="https://www.100jsprojects.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            100 JavaScript Projects
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+      </div>
+    </div>
+  );
+}
