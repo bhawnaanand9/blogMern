adc922a83264fcc1998ccaf50082734964a042e2
bhawnaanand9 <bhawnaaaa09@gmail.com>
2024-07-07 13:24:13 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 
+import PostPage from "./pages/PostPage";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -30,6 +32,7 @@ const App = () => {
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
       </Routes>
       <FooterComponent />
     </BrowserRouter>
